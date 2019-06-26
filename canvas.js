d6fd87c2b4e7806f906f324e412493b86fb54358
Karthikeyan <akashkarthiofficial@gmail.com>
2019-06-27 01:03:48 +0530

    canvad.js event list

--- canvas.js
+++ canvas.js
@@ -6,8 +6,29 @@ window.addEventListener("load", () => {
     canvas.height=window.innerHeight;
     canvas.width=window.innerWidth;
     
+    //variables
+    let painting =false;
+    function startPosition(e){
+        painting=true;
+        draw(e);
+    }
+    function finshedPosition(){
+        painting=false;
+        ctx.beginPath();
+    }
+function draw(e){
+    if(!painting) return;
+    ctx.lineWidth=10;
+    ctx.lineCap="round";
+    ctx.strokeStyle="red";
+    ctx.lineTo(e.clientX,e.clientY);
+    ctx.stroke;
     ctx.beginPath();
-    ctx.moveTo(100,100);
-    ctx.lineTo(200,150);
-    ctx.stroke();
+    ctx.moveTo(e.clientX,e.clientY);
+}    
+ //event listeners
+    canvas.addEventListener("mousedown",startPosition);
+    canvas.addEventListener("mouseup",finshedPosition);
+    canvas.addEventListener("mousemove",draw);
+    
 });
