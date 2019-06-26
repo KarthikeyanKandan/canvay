2c0883b9a0f6d9d489e5756548823c85ceeb143c
Karthikeyan <akashkarthiofficial@gmail.com>
2019-06-27 00:40:02 +0530

    canvas height and width

---
+++ canvas.js
@@ -0,0 +1,10 @@
+window.addEventListener("load", () => {
+   const canvas=document.querySelector("#canvas");
+    const ctx=canvas.getContext("2d");
+    
+    //resize
+    canvas.height=window.innerHeight;
+    canvas.width=window.innerWidth;
+    
+    
+});
